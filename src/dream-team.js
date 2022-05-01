276555f03382df12030d980eed4547f21ad171ac
Roman Maklakov <mclakov@MacBook-Air-Roman.local>
2022-05-01 23:58:36 +0300

    init: 3 done

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,40 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+
+    let nameDT = [];
+
+    if (Array.isArray(members)) {
+        members.map(elem => {
+            if (typeof elem == 'string') {
+                for (let char of elem) {
+                    if (char != " ") {
+                        nameDT.push(char.toUpperCase());
+                        break;
+                    }
+                }
+            }
+        })
+
+        return nameDT.sort().join('')
+    }
+
+    return false;
 }
 
 module.exports = {
-  createDreamTeam
+    createDreamTeam
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,55 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+
+    if (date === undefined) {
+        return "Unable to determine the time of year!"
+    }
+
+    try {
+
+        let arrKeys = Object.keys(date);
+
+        if (arrKeys.length > 0) {
+            throw new Error("Invalid date!")
+        }
+
+        else {
+            let month = date.getMonth();
+
+            if (month >= 2 && month < 5) {
+                return "spring"
+            }
+            if (month >= 5 && month < 8) {
+                return "summer"
+            }
+            if (month >= 8 && month < 11) {
+                return "fall"
+            }
+            if (month === 11 || month === 0 || month === 1) {
+                return "winter"
+            }
+        }
+
+
+    } catch {
+        throw new Error("Invalid date!")
+    }
+
+
 }
 
 module.exports = {
-  getSeason
+    getSeason
 };
