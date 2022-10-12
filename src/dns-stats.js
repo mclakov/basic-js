20150efa0b9714bd69934510cc8205f101b96332
Roman Maklakov <mclakov@MR-MacBook.local>
2022-10-13 01:18:41 +0300

    feat: dns-stats task done

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+	console.log('domains', domains);
+	if(domains.length == 0){
+		return {};
+	} else {
+		let arr = [];
+		domains.map((elem, i) => {
+			arr.push('.' + elem.split('.').reverse().join('.'));
+		})
+		let arr2 = domains[0].split('.').reverse();
+		arr.push('.' + arr2[0])
+		let obj = {};
+		arr.map((elem, i) => {
+			let temp = elem.split('.').reverse().join('.');
+			temp = temp.slice(0, -1);
+			let count = 0;
+			for (let j=0; j<domains.length; j++){
+				// console.log('temp', temp);
+				if (domains[j].indexOf(temp, 0) != -1) {
+					// console.log('count', count);
+					count = count +1;
+				}
+			}
+			obj[elem] = count;
+		})
+		console.log('obj = ', obj)
+		return obj;
+	}
 }
 
 module.exports = {
-  getDNSStats
+	getDNSStats
 };
